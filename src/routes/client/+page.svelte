637d5d46ea224278a5459f842e05e7f3a2985645
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-18 19:18:42 +0800

    done UI for 320 breakpoint

--- src/routes/client/+page.svelte
+++ src/routes/client/+page.svelte
@@ -6,12 +6,23 @@
 	import client_boy_icon from '$lib/assets/client_boy_icon.svg';
 	import client_girl_icon from '$lib/assets/client_girl_icon.svg';
 	import DropDown from '$lib/components/general-component/drop-down.svelte';
+	import { fade } from 'svelte/transition';
 
 	export let data: PageServerData;
 
 	if (data.user) setUser(data.user);
 
 	const user = getUser();
+
+	let clientRouteControls = {
+		activeItem: 'Purchase History'
+	};
+
+	let buttonControls = ['Purchase History', 'Payment History'];
+
+	const handleSelect = (selected: string) => {
+		clientRouteControls.activeItem = selected;
+	};
 </script>
 
 <div class=" px-[24px] pt-[29px]">
@@ -48,23 +59,42 @@
 	<hr class="mt-[21px] w-full border-[1px] border-subWhite" />
 
 	<div class="mt-[21px] flex items-center gap-[4px]">
-		<button class="flex h-[22px] w-full items-center justify-center bg-black text-[12px] text-white"
-			>Purchase History</button
-		>
-
-		<button
-			class="flex h-[22px] w-full items-center justify-center bg-lowWhite text-[12px] text-black"
-			>Payment History</button
-		>
+		{#each buttonControls as buttonName}
+			<button
+				on:click={() => handleSelect(buttonName)}
+				class="flex h-[22px] w-full items-center justify-center text-[12px] {clientRouteControls.activeItem ===
+				buttonName
+					? 'bg-black text-white'
+					: 'bg-lowWhite text-black'} ">{buttonName}</button
+			>
+		{/each}
 	</div>
 
-	<div class="mt-[26px] flex flex-col gap-[10px]">
-		{#each Array(20) as sampleArray}
-			<DropDown>
-				{#each Array(10) as sample}
-					<p class="text-[10px]">Chicken - 120 Php</p>
+	<div class="mt-[26px]">
+		{#if clientRouteControls.activeItem === 'Purchase History'}
+			<div class="flex flex-col gap-[10px]" in:fade>
+				{#each Array(20) as sampleArray}
+					<DropDown>
+						{#each Array(10) as sample}
+							<p class="text-[10px]">Chicken - 120 Php</p>
+						{/each}
+					</DropDown>
 				{/each}
-			</DropDown>
-		{/each}
+			</div>
+		{:else}
+			<div class="flex flex-col gap-[10px]" in:fade>
+				{#each Array(20) as sampleArray}
+					<DropDown>
+						{#each Array(10) as sample}
+							<DropDown>
+								{#each Array(20) as sampleee}
+									<p class="text-[10px]">Chicken - 120 Php</p>
+								{/each}
+							</DropDown>
+						{/each}
+					</DropDown>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
