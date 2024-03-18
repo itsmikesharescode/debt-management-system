3762283cd269af5ab0073c1a59110b4375484a72
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-18 22:36:00 +0800

    add UI for client account settings

--- src/lib/components/client-component/client-account.svelte
+++ src/lib/components/client-component/client-account.svelte
@@ -2,8 +2,9 @@
 	import { enhance } from '$app/forms';
 	import type { ResultModel } from '$lib/types';
 	import type { SubmitFunction } from '@sveltejs/kit';
+	import admin_arrowleft_icon from '$lib/assets/admin_arrowleft_icon.svg';
 
-	const logoutActionNews: SubmitFunction = () => {
+	const updateAccountActionNews: SubmitFunction = () => {
 		return async ({ result, update }) => {
 			const { status } = result as ResultModel<{ msg: string }>;
 
@@ -16,32 +17,59 @@
 
 				case 401:
 					break;
-
-				case 403:
-					break;
-
-				case 404:
-					break;
-
-				case 409:
-					break;
-
-				default:
-					break;
 			}
 			await update();
 		};
 	};
 </script>
 
-<form
-	method="post"
-	action="?/logoutAction"
-	enctype="multipart/form-data"
-	use:enhance={logoutActionNews}
->
+<form>
 	<button
 		class="flex h-[35px] w-full items-center justify-center rounded-[10px] bg-black text-[12px] font-semibold text-white active:bg-opacity-60"
 		>Account</button
 	>
 </form>
+
+<div class="fixed bottom-0 left-0 right-0 top-0 bg-overlay">
+	<div class="relative mx-auto mt-[101px] min-h-[362px] w-[255px] bg-white p-2">
+		<div class="relative flex items-center justify-center">
+			<div class="absolute left-0">
+				<button class="flex items-center p-2">
+					<img src={admin_arrowleft_icon} class="" alt="arrowleft-icon" />
+				</button>
+			</div>
+			<p class="text-center text-[12px] font-semibold">Account Settings</p>
+		</div>
+
+		<hr class="mt-[10px] w-full border-[1px] border-subWhite" />
+
+		<div class="mt-[26px] flex items-center justify-center text-center text-[10px] font-semibold">
+			<p class=" w-[198px]">We recommend to change your password every week.</p>
+		</div>
+
+		<div class="mt-[30px] flex flex-col gap-[13px]">
+			<label>
+				<span class="text-[10px] font-semibold">New Password</span>
+				<input
+					type="password"
+					placeholder="Enter your new password"
+					class="h-[35px] w-full rounded-[10px] border-[1px] border-black px-[15px] text-[10px] outline-none"
+				/>
+			</label>
+
+			<label>
+				<span class="text-[10px] font-semibold">Confirm New Password</span>
+				<input
+					type="password"
+					placeholder="Confirm your new password"
+					class="h-[35px] w-full rounded-[10px] border-[1px] border-black px-[15px] text-[10px] outline-none"
+				/>
+			</label>
+
+			<button
+				class="flex h-[35px] items-center justify-center rounded-[10px] bg-black text-[12px] font-semibold text-white"
+				>Update Password</button
+			>
+		</div>
+	</div>
+</div>
