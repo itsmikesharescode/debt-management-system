b55d8a2960e7c40ef295c18297f0b10c3ddf514a
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-19 19:41:22 +0800

    add user list types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,10 @@
-
+export type UserListTB = {
+    id: number
+    created_at: string
+    user_id: string
+    user_fullname: string
+    gender: string
+}
 
 export type ResultModel<T> = {
     status: number
