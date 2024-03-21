8250c9c770a5553125ee0e630c2648203c5a95f0
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-21 13:39:29 +0800

    add server form actions

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -205,5 +205,17 @@ export const actions: Actions = {
 
             return fail(200, { paymentList: newPaymentHistoryList });
         }
+    },
+
+    updateInformationAction: async ({ locals: { supabase }, request }) => {
+        const formData = Object.fromEntries(await request.formData());
+
+        try {
+
+        } catch (error) {
+            const zodError = error as ZodError;
+            const { fieldErrors } = zodError.flatten();
+            return fail(400, { errors: fieldErrors });
+        }
     }
 };
