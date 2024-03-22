3a12b26d941da27befb58c89d4240983038e806d
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-22 13:24:00 +0800

    fix layout for admin create account

--- src/lib/components/admin-component/add-account.svelte
+++ src/lib/components/admin-component/add-account.svelte
@@ -77,7 +77,7 @@
 			action="?/createAccountAction"
 			enctype="multipart/form-data"
 			use:enhance={createAccountActionNews}
-			class="mx-auto mt-[101px] min-h-[383px] w-[255px] bg-white pb-[50px] pt-[10px] sm:w-[416px]"
+			class=" mt-[101px] min-h-[383px] w-full bg-white pb-[50px] pt-[10px] sm:w-[416px]"
 			in:scale
 		>
 			<div class="relative flex items-center justify-center">
@@ -86,7 +86,7 @@
 						<img src={admin_arrowleft_icon} class="" alt="arrowleft-icon" />
 					</button>
 				</div>
-				<p class="text-center text-[12px] font-semibold">Create Account</p>
+				<p class="text-center text-[20px] font-semibold">Create Account</p>
 			</div>
 
 			<hr class="mt-[11px] w-full border-[1px] border-subWhite" />
@@ -94,76 +94,76 @@
 			<div class="mx-[12px] mt-[20px] flex flex-col gap-[6px]">
 				<div class="">
 					<div class="flex items-center gap-[5px]">
-						<label class="flex items-center gap-[1px]">
-							<span class="text-[10px] font-semibold">Male</span>
+						<label class="flex items-center gap-[4px]">
+							<span class="text-[14px] sm:text-[16px]">Male</span>
 							<input type="radio" class="" value="male" name="gender" />
 						</label>
 
-						<label class="flex items-center gap-[1px]">
-							<span class="text-[10px] font-semibold">Female</span>
+						<label class="flex items-center gap-[4px]">
+							<span class="text-[14px] sm:text-[16px]">Female</span>
 							<input type="radio" class="" value="female" name="gender" />
 						</label>
 					</div>
 					{#each actionFormErrors?.gender ?? [] as errorMsg}
-						<p class="text-[10px] font-semibold text-red" in:fade>{errorMsg}</p>
+						<p class="text-[14px] text-red sm:text-[16px]" in:fade>{errorMsg}</p>
 					{/each}
 				</div>
 
 				<label>
-					<span class="text-[10px] font-semibold">Complete Name</span>
+					<span class="text-[14px] sm:text-[16px]">Complete Name</span>
 					<input
 						name="completeName"
 						type="text"
-						class="h-[35px] w-full rounded-[10px] border-[1px] border-black px-[15px] text-[10px] outline-none"
+						class="w-full rounded-[10px] border-[1px] border-black px-[15px] py-[8.5px] text-[14px] outline-none sm:text-[16px]"
 						placeholder="Enter complete name"
 					/>
 					{#each actionFormErrors?.completeName ?? [] as errorMsg}
-						<p class="text-[10px] font-semibold text-red" in:fade>{errorMsg}</p>
+						<p class="text-[14px] text-red sm:text-[16px]" in:fade>{errorMsg}</p>
 					{/each}
 				</label>
 
 				<label>
-					<span class="text-[10px] font-semibold">Email Address</span>
+					<span class="text-[14px] sm:text-[16px]">Email Address</span>
 					<input
 						name="email"
 						type="email"
-						class="h-[35px] w-full rounded-[10px] border-[1px] border-black px-[15px] text-[10px] outline-none"
+						class="w-full rounded-[10px] border-[1px] border-black px-[15px] py-[8.5px] text-[14px] outline-none sm:text-[16px]"
 						placeholder="Enter email address"
 					/>
 					{#each actionFormErrors?.email ?? [] as errorMsg}
-						<p class="text-[10px] font-semibold text-red" in:fade>{errorMsg}</p>
+						<p class="text-[14px] text-red sm:text-[16px]" in:fade>{errorMsg}</p>
 					{/each}
 				</label>
 
 				<label>
-					<span class="text-[10px] font-semibold">Password</span>
+					<span class="text-[14px] sm:text-[16px]">Password</span>
 					<input
 						name="password"
 						type="password"
-						class="h-[35px] w-full rounded-[10px] border-[1px] border-black px-[15px] text-[10px] outline-none"
+						class="w-full rounded-[10px] border-[1px] border-black px-[15px] py-[8.5px] text-[14px] outline-none sm:text-[16px]"
 						placeholder="Enter password"
 					/>
 					{#each actionFormErrors?.password ?? [] as errorMsg}
-						<p class="text-[10px] font-semibold text-red" in:fade>{errorMsg}</p>
+						<p class="text-[14px] text-red sm:text-[16px]" in:fade>{errorMsg}</p>
 					{/each}
 				</label>
 
 				<label>
-					<span class="text-[10px] font-semibold">Confirm Password</span>
+					<span class="text-[14px] sm:text-[16px]">Confirm Password</span>
 					<input
 						name="confirmPassword"
 						type="password"
-						class="h-[35px] w-full rounded-[10px] border-[1px] border-black px-[15px] text-[10px] outline-none"
+						class="w-full rounded-[10px] border-[1px] border-black px-[15px] py-[8.5px] text-[14px] outline-none sm:text-[16px]"
 						placeholder="Confirm password"
 					/>
 					{#each actionFormErrors?.confirmPassword ?? [] as errorMsg}
-						<p class="text-[10px] font-semibold text-red" in:fade>{errorMsg}</p>
+						<p class="text-[14px] text-red sm:text-[16px]" in:fade>{errorMsg}</p>
 					{/each}
 				</label>
 
 				<button
 					disabled={createAccountLoader}
-					class="flex h-[35px] w-full items-center justify-center rounded-[10px] bg-black text-[12px] font-semibold text-white"
+					class="mt-[10px] flex w-full items-center justify-center rounded-[10px] bg-black py-[8.5px] text-[14px] text-white sm:text-[16px]"
 				>
 					<Loader name="Create Account" loader={createAccountLoader} loaderName="Creating..." />
 				</button>
