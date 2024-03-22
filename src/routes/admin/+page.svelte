57411128cf5372516a73d1be7fd014c0c152d049
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-22 18:05:43 +0800

    add search function for admin

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,20 +1,39 @@
 <script lang="ts">
-	import { setUser, getUser } from '$lib';
+	import { setUser, getUser, createSearchStore, searchHandler } from '$lib';
 	import Account from '$lib/components/admin-component/account.svelte';
 	import AddAccount from '$lib/components/admin-component/add-account.svelte';
 	import ClientFolder from '$lib/components/admin-component/client-folder.svelte';
 	import Logout from '$lib/components/admin-component/logout.svelte';
 	import Search from '$lib/components/admin-component/search.svelte';
+	import { onDestroy } from 'svelte';
 	import type { PageServerData } from './$types';
+	import { flip } from 'svelte/animate';
+	import { fade } from 'svelte/transition';
 
 	export let data: PageServerData;
 
 	if (data.user) setUser(data.user);
 
 	const user = getUser();
+
+	/* $: transactions = $adminState.purchaseArray?.map( (purchase) => ({
+        ...purchase,
+        searchTerms: `${purchase.purchase_products_with_price} ${dateConvert2(purchase.created_at)}`
+    })) */
+
+	const clientList = data.clientList?.map((client) => ({
+		...client,
+		searchTerms: `${client.user_fullname} ${client.user_email}`
+	}));
+
+	const searchStore = createSearchStore(clientList ?? []);
+
+	const unsubscribe = searchStore.subscribe((model) => searchHandler(model));
+
+	onDestroy(() => unsubscribe());
 </script>
 
-<div class="xs:px-[35px] px-[23px] pt-[17px] lg:mx-auto lg:w-[1349px]">
+<div class="px-[23px] pt-[17px] xs:px-[35px] lg:mx-auto lg:w-[1349px]">
 	<div class="flex items-center justify-between">
 		<div class="flex gap-[5px]">
 			<div class="w-[104px]">
@@ -38,19 +57,19 @@
 		</div>
 
 		<div class="hidden sm:flex">
-			<Search />
+			<Search bind:searchValue={$searchStore.search} />
 		</div>
 	</div>
 
 	<hr class=" mt-[12px] w-full border-[1px] border-subWhite sm:mt-[23px]" />
 
 	<div class="my-[16px] sm:hidden">
-		<Search />
+		<Search bind:searchValue={$searchStore.search} />
 	</div>
 
 	<div class="flex flex-col gap-[10px] sm:mt-[28px] sm:grid sm:grid-cols-2 lg:grid-cols-4 lg:gap-4">
-		{#each data?.clientList ?? [] as client}
-			<div class="">
+		{#each $searchStore.filtered as client (client.id)}
+			<div class="" transition:fade animate:flip={{ duration: 300 }}>
 				<ClientFolder {client} />
 			</div>
 		{/each}
